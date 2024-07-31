70192631abe7cc88772075fd68532d90954df61a
tdlgsr11011 <atharvjoshi27@gmail.com>
2024-07-31 11:37:10 +0530

    Allowing CORS

--- todo-service/index.js
+++ todo-service/index.js
@@ -1,8 +1,14 @@
+const cors = require("cors");
 const express = require("express");
 const { config } = require("dotenv");
 
 const { connectDB } = require("./database");
 const { todosRouter } = require("./routes/todo");
+
+const corsOptions = {
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+};
 
 config();
 
@@ -16,6 +22,7 @@ connectDB(process.env.DB_URL)
     console.log("Error while conneting DB");
   });
 
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
