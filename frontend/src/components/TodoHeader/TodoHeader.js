e70c379510e05c0a3d26600b9d187abca15086cf
tdlgsr11011 <atharvjoshi27@gmail.com>
2024-08-13 09:40:21 +0530

    proper styling

--- frontend/src/components/TodoHeader/TodoHeader.js
+++ frontend/src/components/TodoHeader/TodoHeader.js
@@ -6,7 +6,9 @@ const TodoHeader = (props) => {
   return (
     <header className={styles.TodoHeader}>
       <h1>Todo App</h1>
-      <button onClick={props.openModal}>Create Todo</button>
+      <button onClick={props.openModal} className={styles.CreateTodoButton}>
+        Create Todo
+      </button>
     </header>
   );
 };
