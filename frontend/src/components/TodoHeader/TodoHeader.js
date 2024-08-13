3620bad51762dd5083ab8677b0b78e17664c2af3
tdlgsr11011 <atharvjoshi27@gmail.com>
2024-08-13 12:06:05 +0530

    button styles

--- frontend/src/components/TodoHeader/TodoHeader.js
+++ frontend/src/components/TodoHeader/TodoHeader.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 import styles from "./TodoHeader.module.scss";
+import buttonStyles from "../../styles/Button.module.scss";
 
 const TodoHeader = (props) => {
   return (
     <header className={styles.TodoHeader}>
       <h1>Todo App</h1>
-      <button onClick={props.openModal} className={styles.CreateTodoButton}>
+      <button
+        onClick={props.openModal}
+        className={`${styles.CreateTodoButton} ${buttonStyles.default}`}
+      >
         Create Todo
       </button>
     </header>
