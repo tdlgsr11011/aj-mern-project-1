fdc8b1fad0aba682592ee1ce118e922a19428532
tdlgsr11011 <atharvjoshi27@gmail.com>
2024-08-15 00:04:43 +0530

    restructuring

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ app.get("/", (req, res) => res.redirect("/todos"));
 app.use("/todos", todosRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("../frontend/build"));
+  app.use(express.static("./frontend/build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "./frontend", "build", "index.html"));
   });
 }
 
