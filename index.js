0039573cb98d1f19024683f5c00840fe761e49f0
tdlgsr11011 <atharvjoshi27@gmail.com>
2024-08-15 11:53:15 +0530

    try to fix

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ if (process.env.NODE_ENV == "development") {
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => res.redirect("/todos"));
+// app.get("/", (req, res) => res.redirect("/todos"));
 
 app.use("/todos", todosRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("./frontend/build"));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
   });
